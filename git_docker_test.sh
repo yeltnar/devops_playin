ee1f752c78652d16435d59afaa6190ad356cd312
Drew Brantley <brantley@us.ibm.com>
2019-10-09 13:03:32 -0500

    first

---
+++ git_docker_test.sh
@@ -0,0 +1,12 @@
+origin_url=$(git config --get remote.origin.url)
+repo_name=$(basename `git rev-parse --show-toplevel`)
+branch=master
+
+folder=$repo_name+$branch
+
+cd /tmp
+git clone $origin_url $folder
+cd $folder
+git checkout $branch
+
+npm run docker
